829bc0f1e6f0d5a1a10339c1773bdb1ce3f73161
AngeLauri1501 <anla223@vidalibarraquer.net>
2024-01-16 09:22:51 +0100

    ApiDoc con Swagger Añadido

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,12 +4,32 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const transporter = require("./mailer");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 require("dotenv").config();
 
 const app = express();
 const port = 3001;
 const server = createServer(app);
 const io = new Server(server);
+
+// Swagger configuration
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "My API",
+            version: "1.0.0",
+            description: "API documentation with Swagger",
+        },
+    },
+    apis: ["./index.js"], // Especifica la ruta donde se encuentran tus rutas
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+// Middleware para servir la documentación Swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use(cors());
 app.use(express.static("public"));
@@ -19,12 +39,11 @@ io.on("connection", (socket) => {
 });
 
 async function sendEmail() {
-
     let mailOptions = {
         from: process.env.GMAIL_USER,
         to: process.env.GMAIL_USER,
         subject: "User Connected",
-        text: "A user has connected to the server."
+        text: "A user has connected to the server.",
     };
 
     // Send the email
@@ -36,7 +55,26 @@ async function sendEmail() {
     }
 }
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get a list of users
+ *     description: Retrieve a list of users from the server
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: List of users retrieved successfully
+ *               users: []
+ */
+app.get("/api/users", (req, res) => {
+    // Tu lógica para obtener la lista de usuarios
+    res.json({ message: "List of users retrieved successfully", users: [] });
+});
+
 server.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
-    sendEmail();
 });
