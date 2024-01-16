01df618be5125c328d3a7b07c91d143da83f35b2
YaizaPG <yaiza.paez.garcia@gmail.com>
2024-01-16 13:27:20 +0100

    Correu token

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -58,7 +58,24 @@ async function sendVerificationEmail(email) {
         to: email,
         subject: "Verification token",
         text: "",
-        html: `<div style='font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2'> <div style='margin:50px auto;width:70%;padding:20px 0'> <div style='border-bottom:1px solid #eee'> <a href='' style='font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600'>Pajoot</a> </div> <p style='font-size:1.1em'>Hi,</p> <p>Thank you for choosing Pajoot. Use the following OTP to complete your Sign Up procedures. OTP is valid for 15 minutes</p> <h2 style='background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;'>${token}</h2> <p style='font-size:0.9em;'>Regards,<br />Pajoot team</p> <hr style='border:none;border-top:1px solid #eee' /> <div style='float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300'> <p>Pajoot Inc</p> <p>1600 Amphitheatre Parkway</p> <p>California</p> </div> </div> </div> `
+        html: `
+        <div style='font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2'>
+            <div style='margin:50px auto;width:70%;padding:20px 0'>
+               <div style='border-bottom:1px solid #eee'>
+                    <a href='' style='font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600'><img src="https://i.ibb.co/Wx3gTMF/logo-pajoot.png"></a>
+                </div>
+                <p style='font-size:1.1em'>Holi,</p>
+                <p>Gràcies per escollir Pajoot. Utilitza el següent codi per completar l'accés. Aquest codi serà vàlid durant 15 minuts.</p>
+                <h2 style='background: #1688A1;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;'>${token}</h2>
+                <p style='font-size:0.9em;'>Atentament,<br />l'equip de Pajoot.</p> <hr style='border:none;border-top:1px solid #eee' />
+                <div style='float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300'>
+                    <p>Ins (ViB) Dpt Pajoot</p>
+                    <p>Rambla President Lluís Companys, 3</p>
+                    <p>43005 Tarragona</p>
+                </div>
+            </div>
+        </div>
+        `
     };
 
     // Send the email
