988980da7897df764275f4f1e42238c0c1f9b906
AngeLauri1501 <anla223@vidalibarraquer.net>
2024-01-16 13:04:21 +0100

    prueba del router

---
+++ FrontEnd/src/pages/login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <>
+        <div className="entry-container">
+            <img src='./assets/img/logo-pajoot.png' alt="Logo-Pajoot" />
+            <div className="entry-credentials">
+                <form method='POST' className="form-login" action=''>
+                    <input type='email' className="form-login_input" name='correu' placeholder="Email" />
+                    <input type="password" className="form-login_input" name="contrasenya" placeholder="Contraseña" />
+                    <input type='submit' className="form-login_button" value='acceder' />Acceder
+                </form>
+            </div>
+            <a href="" className="login-google" ><img src="./assets/img/logo-google.png" alt="Logo-Google" /></a>
+            <p>o</p>
+            <a href="" className="login-anonim">Acceder de manera anónima</a>
+        </div>
+        </>
+    );
+};
+
+export default Login;
