85412f7a34928567da4c92fc553ea730dc8dd456
AngeLauri1501 <anla223@vidalibarraquer.net>
2024-01-29 09:04:30 +0100

    verificacion token front

--- FrontEnd/src/pages/verify.jsx
+++ FrontEnd/src/pages/verify.jsx
@@ -8,8 +8,9 @@ import { useNavigate } from 'react-router-dom';  // Importa useNavigate
 const Verify = () => {
     const { userEmail, setUserEmail } = useStore();
     const [error, setError] = useState(null);
-    const [otp, setOtp] = useState('');
+    const [otp, setOtp] = useState(0);
     const [targetDate, setTargetDate] = useState(Date.now() + 900000);
+    const [lastResendTime, setLastResendTime] = useState(0);
     const navigate = useNavigate();  // Obtén la función de navegación
 
     // Función para formatear la salida del contador
@@ -26,12 +27,93 @@ const Verify = () => {
         }
     }, [userEmail, navigate]);
 
+    const handleTokenResend = async (event) => {
+        event.preventDefault();
+
+        const currentTime = Date.now();
+
+        if (currentTime - lastResendTime < 60000) {
+            setError('Por favor, espere antes de intentar reenviar el token nuevamente.');
+            return;
+        }
+
+        try {
+            // Realizar la petición POST a la API con el email
+            const response = await fetch('http://localhost:3001/api/send-verification-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: userEmail,
+                }),
+            });
+
+            // Verificar la respuesta de la API y manejarla según tus necesidades
+            if (response.ok) {
+                // La petición fue exitosa
+                console.log('Email de verificación reenviado con éxito.');
+                setTargetDate(Date.now() + 900000);
+                setLastResendTime(currentTime);
+            } else {
+                // La petición no fue exitosa, manejar el error según tus necesidades
+                console.error('Error al reenviar el email de verificación.');
+            }
+        } catch (error) {
+            // Manejar errores de la petición (p.ej., error de red)
+            console.error('Error en la petición POST:', error.message);
+        }
+    };
+
+    const verifyToken = async () => {
+        try {
+            // Realizar la petición POST a la API con el email y el token
+            const response = await fetch('http://localhost:3001/api/verify-token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: userEmail,
+                    verification_token: otp,
+                }),
+            });
+
+            // Verificar la respuesta de la API y manejarla según tus necesidades
+            if (response.ok) {
+                // La petición fue exitosa
+                console.log('Token verificado con éxito.');
+            }
+            else {
+                // La petición no fue exitosa, manejar el error según tus necesidades
+                throw new Error('Error verificando el token.');
+            }
+        } catch (error) {
+            // Manejar errores de la petición (p.ej., error de red)
+            throw error;
+        }
+    }
+
+    const mutation = useMutation(verifyToken, {
+        onSuccess: (data) => {
+            console.log('Token verified successfully:');
+        },
+        onError: (error) => {
+            console.error('Error verifying token:', error);
+        },
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        mutation.mutate();
+    };
+
     return (
         <>
             <div className="entry-container new-register">
                 <h1>Nuevo Usuario</h1>
                 <div className="entry-credentials new-credentials">
-                    <form className="form-login form-verify">
+                    <form className="form-login form-verify" onSubmit={handleSubmit}>
                         <p>Introduce el código de validación</p>
                         <div className="form-verify_countdown">
                             <Countdown date={targetDate} renderer={({ minutes, seconds }) => formatTime({ minutes, seconds })} />
@@ -46,12 +128,12 @@ const Verify = () => {
                             required
                         />
                         {error && <p className="error-message">{error}</p>}
-                        <button type='submit' className="form-login_button form-verify_button">
-                            Validar
+                        <button type='submit' className="form-login_button form-verify_button" disabled={mutation.isLoading}>
+                            {mutation.isLoading ? 'Validando...' : 'Validar'}
                         </button>
                     </form>
                 </div>
-                <p className="register-text">No ha llegado el código?</p><br /><a href="" className="register-new">Volver a enviar</a>
+                <p className="register-text">No ha llegado el código?</p><br /><a href="" onClick={handleTokenResend} className="register-new">Volver a enviar</a>
             </div>
         </>
     );
