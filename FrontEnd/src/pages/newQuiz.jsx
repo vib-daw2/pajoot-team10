64bb1a818ed30894c262d632c8839bd23da7e216
YaizaPG <yaiza.paez.garcia@gmail.com>
2024-03-11 09:39:36 +0100

    Estilo Choose

--- FrontEnd/src/pages/newQuiz.jsx
+++ FrontEnd/src/pages/newQuiz.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect} from 'react';
 import { socket } from '../socket';
 import useStore from '../store';
+import app from '../../firebaseConfig';
+import { getAuth, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom'; 
 
 const NewQuiz = () => {
-
+  const {userLogged, setUserLogged, game, setGame} = useStore();
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
-  const { game, setGame } = useStore();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const auth = getAuth(app);
 
   useEffect(() => {
     socket.on('gameCreated', (game) => {
@@ -14,21 +18,79 @@ const NewQuiz = () => {
       setGame(game);
       navigate('/host');
     });
+    const handleClickOutside = (event) => {
+      if (!event.target.closest('.dropdown')) {
+          setDropdownOpen(false);
+      }
+    };
+
+    // Agregar event listener al montar el componente
+    document.addEventListener('click', handleClickOutside);
+
+    // Limpiar event listener al desmontar el componente
+    return () => {
+        document.removeEventListener('click', handleClickOutside);
+    };
   }
   , []);
 
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+};
+
+const handleLogout = () => {
+  // Cerrar sesión
+  console.log('Cerrando sesión..');
+
+  auth.signOut();
+  setUserLogged(null);
+  
+  navigate('/');
+};
+
 
   return (
-    <div>
-        <h2>Empieza un pajoot!</h2>
-        <h3>Elige una tematica</h3>
-        <button onClick={() => socket.emit('createGame',JSON.stringify({tematica:'musica'}))}>Musica</button>
-        <button onClick={() => socket.emit('createGame',JSON.stringify({tematica:'programacion'}))}>Programacion</button>
-        <button onClick={() => socket.emit('createGame',JSON.stringify({tematica:'cine'}))}>Cine</button>
-        <button onClick={() => socket.emit('createGame',JSON.stringify({tematica:'actualidad'}))}>Actualidad</button>
-        <span>o</span>
-        <button>Crea tu Propio Pajoot!</button>
-    </div>
+    <>
+      <div className='menu-top'>
+      <a href='/'>
+          <img src='./assets/img/logo-pajoot.png' className="logo-pajoot" alt="Logo-Pajoot" />
+      </a>
+      {!userLogged && (
+      <div className='user-info'>
+          <a href='/login' className='login-link'>Iniciar sesión</a>
+          <img src='./assets/img/usuario-de-perfil.png' className='user-avatar' alt='Avatar-Usuario' />
+      </div>
+      )}
+      {userLogged && (
+      <div className='user-info'>
+          <div className={`dropdown ${dropdownOpen ? 'active' : ''}`} onClick={toggleDropdown}>
+              <button className='dropbtn'>
+                  <p id='user-display-name' className='login-link'>{userLogged.displayName}</p>
+                  <img src={userLogged.photoURL === null ? './assets/img/usuario-de-perfil.png' : userLogged.photoURL} className='user-avatar' alt='Avatar-Usuario' />
+              </button>
+              <div className='dropdown-content'>
+                  <div className='triangle'></div>
+                  <a href='/profile'>Editar Perfil</a>
+                  <a href='' onClick={handleLogout}>Cerrar Sesión</a>
+              </div>
+          </div>
+      </div>
+      )}
+      </div>
+      <div className='choose-container'>
+          <h1>Iniciar un Pajoot</h1>
+          <div className='choose-content'>
+            <div className='choose-container_buttons'>
+              <button className='choose-button' onClick={() => socket.emit('createGame',JSON.stringify({tematica:'musica'}))}>Música</button>
+              <button className='choose-button' onClick={() => socket.emit('createGame',JSON.stringify({tematica:'programacion'}))}>Programación</button>
+              <button className='choose-button' onClick={() => socket.emit('createGame',JSON.stringify({tematica:'cine'}))}>Cine</button>
+              <button className='choose-button' onClick={() => socket.emit('createGame',JSON.stringify({tematica:'actualidad'}))}>Actualidad</button>
+            </div>
+            <p>o</p>
+            <button className='create-button'>Crear Nuevo</button>
+          </div>
+      </div>
+    </>
   );
 };
 
