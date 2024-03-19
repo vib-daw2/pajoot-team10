01057c05b84b362cbeb58c2e006836f3bae4a5d3
YaizaPG <yaiza.paez.garcia@gmail.com>
2024-03-19 10:11:49 +0100

    Ranking

--- FrontEnd/src/components/hostTimeUp.jsx
+++ FrontEnd/src/components/hostTimeUp.jsx
@@ -12,20 +12,25 @@ const HostTimeUp= () => {
   const sortedPlayers = players.sort((a, b) => b.gameData.score - a.gameData.score);
    
   return (
+    <>
+    <p className='time-out'>Tiempo Finalizado!</p>
     <div className='question-container ranking-container'>
-        <h2>Tiempo Finalizado</h2>
+      <div className='ranking-content'>
         <h1>Clasificación</h1>
-        <ul>
+        <ul className='ranking-list'>
         {/* Mapear y renderizar los jugadores en orden de puntuación */}
         {sortedPlayers.map((player, index) => (
-          <li key={player.playerId}>
-            <img src={player.photo} alt={player.playerName} style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '10px' }} />
-            {index + 1}. {player.name}: {(player.gameData.score).toFixed()}
+          <li className='ranking-player' key={player.playerId}>
+            <p>{index + 1}.</p>
+            <img className='ranking-image' src={player.photo} alt={player.playerName} />
+            {player.name}: {(player.gameData.score).toFixed()}
           </li>
         ))}
-      </ul>
+        </ul>
+      </div>
         <button className='question-button_next' onClick={() => socket.emit('nextQuestion',JSON.stringify({pin:game.pin}))}>Siguiente Pregunta</button>
     </div>
+    </>
   );
 };
 
--- FrontEnd/src/components/profileAvatar.jsx
+++ FrontEnd/src/components/profileAvatar.jsx
@@ -15,16 +15,6 @@ import React from 'react';
         './assets/avatar/gato-12.png',
         './assets/avatar/gato-13.png',
         './assets/avatar/gato-14.png',
-        './assets/avatar/humano-1.png',
-        './assets/avatar/humano-2.png',
-        './assets/avatar/humano-3.png',
-        './assets/avatar/humano-4.png',
-        './assets/avatar/humano-5.png',
-        './assets/avatar/humano-6.png',
-        './assets/avatar/humano-7.png',
-        './assets/avatar/humano-8.png',
-        './assets/avatar/humano-9.png',
-        './assets/avatar/humano-10.png'
     ];
     const ProfileAvatar = ({ avatars, onSelect, selectedAvatar }) => {
     
