08a767a08faadb7b8f00ee950b58b2bbd37c19bf
AngeLauri1501 <anla223@vidalibarraquer.net>
2024-04-09 12:01:57 +0200

    respuesta correcta

--- FrontEnd/src/components/playerTimeUp.jsx
+++ FrontEnd/src/components/playerTimeUp.jsx
@@ -35,7 +35,7 @@ const PlayerTimeUp = () => {
             <img src='./assets/gif/cat-no.gif' className='answer-image' alt='Gato negando' />
           )
         )}
-        <p className='answer-correct'>La respuesta correcta es: {question.respuesta}</p>
+        <p className='answer-correct'>La respuesta correcta es: {question.opciones[question.respuesta]}</p>
       </div>
       {mensajeRacha && (
         <div className='answer-streak'>
