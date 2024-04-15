cbd9d3d1e575a04be6f722c9d199a6fc16f6b1ed
AngeLauri1501 <anla223@vidalibarraquer.net>
2024-04-15 13:45:49 +0200

    Easter egg v2

--- FrontEnd/src/components/playerLobby.jsx
+++ FrontEnd/src/components/playerLobby.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useRef} from 'react';
-import { socket } from '../socket';
+import React, { useState, useEffect, useRef } from 'react';
 import useStore from '../store';
 import Lobbysound from '../../public/assets/sounds/lobby-classic-game.mp3';
-
+import Nyancatsound from '../../public/assets/sounds/Nyan-Cat.mp3';
 
 const PlayerLobby = () => {
-  const { game, setGame, userLogged, setUserLogged, muted, setMuted, isMuted, setIsMuted} = useStore();
-  const audioRef = useRef(false);
+  const { game, userLogged, muted, setMuted, isMuted, setIsMuted } = useStore();
+  const audioRef = useRef(null);
 
   useEffect(() => {
     if (audioRef.current && !muted) {
@@ -18,8 +17,7 @@ const PlayerLobby = () => {
     if (audioRef.current) {
       audioRef.current.volume = isMuted ? 0 : 1;
     }
-  }
-  , [isMuted]);
+  }, [isMuted]);
 
   function toggleMute() {
     setMuted(!muted);
@@ -27,25 +25,30 @@ const PlayerLobby = () => {
   }
 
   return (
-    <>
     <div className='lobby-container'>
-      {game && game.remoteMode &&(
-      <>
-      <button onClick={toggleMute}>{muted ? 'Desmutear' : 'Mutear'}</button>
-      <audio id='lobby-music' src={Lobbysound} loop autoPlay ref={audioRef} />
-      </>
+      {game && game.remoteMode && (
+        <>
+          <button onClick={toggleMute}>{muted ? 'Desmutear' : 'Mutear'}</button>
+          {userLogged && userLogged.displayName && userLogged.displayName.toLowerCase() === 'nyan' ? (
+            <audio id='lobby-music' src={Nyancatsound} loop autoPlay ref={audioRef} />
+          ) : (
+            <audio id='lobby-music' src={Lobbysound} loop autoPlay ref={audioRef} />
+          )}
+        </>
       )}
       {userLogged && userLogged.displayName && (
-      <div className='lobby-player'>
-        <img className='lobby-image' src={userLogged.photoURL} alt={userLogged.displayName} />
-        <h1>{userLogged.displayName}</h1>
-      </div>
+        <div className='lobby-player'>
+          <img className='lobby-image' src={userLogged.photoURL} alt={userLogged.displayName} />
+          <h1>{userLogged.displayName}</h1>
+        </div>
       )}
+      {userLogged && userLogged.displayName && userLogged.displayName.toLowerCase() === 'nyan' ? (
+      <img src='./assets/gif/nyan-cat.gif' className='cat-waiting' alt='Cat-waiting' />
+      ) : (
       <img src='./assets/gif/cat-waiting.gif' className='cat-waiting' alt='Cat-waiting' />
+      )}
       <p>Esperando a otros jugadores</p>
-  
     </div>
-    </>
   );
 };
 
