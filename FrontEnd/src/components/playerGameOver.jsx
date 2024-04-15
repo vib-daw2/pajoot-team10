0372a4048afb0a5451b1331d65ad4705be0e856c
AngeLauri1501 <anla223@vidalibarraquer.net>
2024-04-15 12:40:32 +0200

    posicion final

--- FrontEnd/src/components/playerGameOver.jsx
+++ FrontEnd/src/components/playerGameOver.jsx
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { socket } from '../socket';
+import useStore from '../store';
 
 const PlayerGameOver = () => {
+
+  const { game, setGame, question, setQuestion, userLogged, setUserLogged, answeredCorrectly, setAnsweredCorrectly, racha, setRacha, mensajeRacha, setMensajeRacha } = useStore();
+  const [playerPosition, setPlayerPosition] = useState(null);
+
+  useEffect(() => {
+    const players = game.gameData.players.players;
+    const sortedPlayers = players.sort((a, b) => b.gameData.score - a.gameData.score);
+    const userIndex = sortedPlayers.findIndex(player => player.playerId === userLogged.uid);
     
+    if (userIndex !== -1) {
+      // El jugador está en la lista de jugadores
+      const position = userIndex + 1;
+      setPlayerPosition(position);
+    } else {
+      // El jugador no está en la lista de jugadores (por ejemplo, si se desconectó antes del final del juego)
+      setPlayerPosition(null);
+    }
+  }, [game, userLogged]);
+
+  
   return (
     <div className='time-out'>
         <h1>¡Se acabó el juego!</h1>
+        {playerPosition !== null && (
+        <h1>¡Has quedado {playerPosition}º!</h1>
+      )}
     </div>
   );
 };
